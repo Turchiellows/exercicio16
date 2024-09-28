5ef4f04e9f8e4e025631e351ea78b6dd64cec34d
root <root@DELL-7559>
2024-09-28 14:14:28 -0300

    refactor: Mudança de indentação

--- src/main/java/Exercicio16.java
+++ src/main/java/Exercicio16.java
@@ -1,56 +1,52 @@
+// IMPORTS
 
 import java.util.Scanner;
 
 public class Exercicio16 {
 
-    public static void main(String[] args) {
-        // INSTANTIATE
-        Scanner sc = new Scanner(System.in);
-	
-	
-	// DECLARATE
-        double a;
-        double b;
-        int option;
-        
-	
-	// COLLECT DATA
-        System.out.print("A : ");
-        a = sc.nextDouble();
-        System.out.print("B : ");
-        b = sc.nextDouble();
-       
-        
-        // MENU
-        System.out.println("======================================");
-        System.out.println("CHOSE AN OPTION");
-        System.out.println("======================================");
-        System.out.println("1 - ADD");
-        System.out.println("2 - SUB");
-        System.out.println("3 - MUL");
-        System.out.println("4 - DIV");
-	 System.out.print("Your option: ");
-        option = sc.nextInt();
-        
-       
+   public static void main(String[] args) {
+      // INSTANTIATE
+      Scanner sc = new Scanner(System.in);
 
-        // OUTPUT
-        switch (option) {
-            case 1:
-                System.out.println(a + b);
-                break;
-                
-            case 2:
-                 System.out.println(a - b);
-                break;
-                
-            case 3:
-                 System.out.println(a * b);
-                break;
-                
-            default: 
-                 System.out.println(a / b);
-        }
-	
-    }
+      // DECLARATE
+      double a;
+      double b;
+      int option;
+
+      // COLLECT DATA
+      System.out.print("A : ");
+      a = sc.nextDouble();
+      System.out.print("B : ");
+      b = sc.nextDouble();
+
+      // MENU
+      System.out.println("======================================");
+      System.out.println("CHOSE AN OPTION");
+      System.out.println("======================================");
+      System.out.println("1 - ADD");
+      System.out.println("2 - SUB");
+      System.out.println("3 - MUL");
+      System.out.println("4 - DIV");
+      System.out.print("Your option: ");
+      option = sc.nextInt();
+
+      // OUTPUT
+      switch (option) {
+         case 1:
+            System.out.println(a + b);
+            break;
+
+         case 2:
+            System.out.println(a - b);
+            break;
+
+         case 3:
+            System.out.println(a * b);
+            break;
+
+         default:
+            System.out.println(a / b);
+      }
+
+   }
 }
